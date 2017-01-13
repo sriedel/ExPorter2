35df6ee8721ecd2861f6b2785d222811be79e740
Sven Riedel <sr@gimp.org>
2017-01-13 11:27:41 +0100

    Add transforming of leading and vowel ys

--- lib/porter2/word.ex
+++ lib/porter2/word.ex
@@ -1,4 +1,8 @@
 defmodule Porter2.Word do
   def trim_leading_apostrophe( "'"<> rest = word ), do: rest
   def trim_leading_apostrophe( word ), do: word
+
+  def transform_vowel_ys( word ) do
+    Regex.replace( ~r/(^|[aeiou])y/, word, "\\1Y" )
+  end
 end
--- spec/lib/porter2/word_spec.exs
+++ spec/lib/porter2/word_spec.exs
@@ -5,12 +5,35 @@ defmodule Porter2.WordSpec do
 
   describe ".trim_leading_apostrophe" do
     it "should remove a leading apostrophe in a word" do
-      "'cos" |> Porter2.Word.trim_leading_apostrophe |> expect |> to( eq "cos" )
+      "'cos" 
+      |> Porter2.Word.trim_leading_apostrophe 
+      |> expect |> to( eq "cos" )
     end
 
     it "should leave a word as is if it has no apostrophe" do
-      "cos" |> Porter2.Word.trim_leading_apostrophe |> expect |> to( eq "cos" )
+      "cos" 
+      |> Porter2.Word.trim_leading_apostrophe 
+      |> expect |> to( eq "cos" )
     end
-    
+  end
+
+  describe ".transform_vowel_ys" do
+    it "should transform an initial y to Y" do
+      "you" 
+      |> Porter2.Word.transform_vowel_ys 
+      |> expect |> to( eq "You" )
+    end
+
+    it "should transform a y after a vowel to Y" do
+      "oy"
+      |> Porter2.Word.transform_vowel_ys
+      |> expect |> to( eq "oY" )
+    end
+
+    it "should not transform a y after a consonant to Y" do
+      "why"
+      |> Porter2.Word.transform_vowel_ys
+      |> expect |> to( eq "why" )
+    end
   end
 end
