dd29fa279dce3736c4fb12085990337c728b24d3
Sven Riedel <sr@gimp.org>
2017-01-16 11:13:46 +0100

    Add adverb replacement for words ending with 'edly'

--- lib/porter2/word.ex
+++ lib/porter2/word.ex
@@ -9,4 +9,124 @@ defmodule Porter2.Word do
   def remove_apostrophe_s_suffix( word ) do 
     Regex.replace( ~r/'s?'?\z/, word, "" )
   end
+
+  def replace_suffixes( word ) do
+    word
+    |> String.reverse
+    |> replace_reversed_suffixes
+  end
+
+  defp replace_reversed_suffixes( "sess" <> reversed_prefix = word ) do
+    String.reverse( reversed_prefix ) <> "ss"
+  end
+
+  defp replace_reversed_suffixes( "dei" <> reversed_prefix = word ) do
+    new_suffix = case String.length( reversed_prefix ) do
+                   x when x <= 1 -> "ie"
+                   _ -> "i"
+                 end
+    String.reverse( reversed_prefix ) <> new_suffix
+  end
+
+  defp replace_reversed_suffixes( "sei" <> reversed_prefix = word ) do
+    new_suffix = case String.length( reversed_prefix ) do
+                   x when x <= 1 -> "ie"
+                   _ -> "i"
+                 end
+    String.reverse( reversed_prefix ) <> new_suffix
+  end
+
+  defp replace_reversed_suffixes( "s" <> reversed_prefix = word ) do
+    if Regex.match?( ~r/^.[^aeiouy]*$/, reversed_prefix ) do
+      String.reverse( word )
+    else
+      String.reverse( reversed_prefix )
+    end
+  end
+
+  defp replace_reversed_suffixes( "ylde" <> reversed_prefix = word ) do
+    if Regex.match?( ~r/[aeiou]/, reversed_prefix ) do
+      reversed_prefix 
+      |> replace_adverb_suffix
+      |> String.reverse
+    else
+      String.reverse( word )
+    end
+  end
+
+  defp replace_adverb_suffix( "ta" <> _reversed_prefix = word ) do
+    "e" <> word
+  end
+
+  defp replace_adverb_suffix( "lb" <> _reversed_prefix = word ) do
+    "e" <> word
+  end
+
+  defp replace_adverb_suffix( "zi" <> _reversed_prefix = word ) do
+    "e" <> word
+  end
+
+  defp replace_adverb_suffix( "bb" <> reversed_prefix = _word ) do
+    "b" <> reversed_prefix
+  end
+
+  defp replace_adverb_suffix( "dd" <> reversed_prefix = _word ) do
+    "d" <> reversed_prefix
+  end
+
+  defp replace_adverb_suffix( "ff" <> reversed_prefix = _word ) do
+    "f" <> reversed_prefix
+  end
+
+  defp replace_adverb_suffix( "gg" <> reversed_prefix = _word ) do
+    "g" <> reversed_prefix
+  end
+
+  defp replace_adverb_suffix( "mm" <> reversed_prefix = _word ) do
+    "m" <> reversed_prefix
+  end
+
+  defp replace_adverb_suffix( "nn" <> reversed_prefix = _word ) do
+    "n" <> reversed_prefix
+  end
+
+  defp replace_adverb_suffix( "pp" <> reversed_prefix = _word ) do
+    "p" <> reversed_prefix
+  end
+
+  defp replace_adverb_suffix( "rr" <> reversed_prefix = _word ) do
+    "r" <> reversed_prefix
+  end
+
+  defp replace_adverb_suffix( "tt" <> reversed_prefix = _word ) do
+    "t" <> reversed_prefix
+  end
+
+  defp replace_adverb_suffix( word ) do
+    short_word = word
+                 |> String.reverse
+                 |> is_word_short?
+    if short_word do
+      "e" <> word
+    else
+      word
+    end
+  end
+
+  def r1_region( word ) do
+    case Regex.run( ~r/(?<=[aeiou][^aeiou]).*$/, word, caputure: :first ) do
+      [ "" ]              -> nil
+      x when is_list( x ) -> hd( x )
+      _                   -> nil
+    end
+  end
+
+  def word_ends_in_short_syllable?( word ) do
+    Regex.match?( ~r/[^aeiouy][aeiouy][^aeioywxY]$/, word )
+  end
+
+  def is_word_short?( word ) do
+    ( r1_region( word ) == nil ) && word_ends_in_short_syllable?( word )
+  end
+
 end
--- spec/lib/porter2/word_spec.exs
+++ spec/lib/porter2/word_spec.exs
@@ -62,4 +62,288 @@ defmodule Porter2.WordSpec do
       |> expect |> to( eq "his" )
     end
   end
+
+  describe ".replace_suffixes" do
+    it "should replace a 'sses' suffix with 'ss' " do
+      "asses"
+      |> Porter2.Word.replace_suffixes
+      |> expect |> to( eq "ass" )
+    end
+
+    context "words with an 'ied' suffix" do
+      it "should replace the word 'ied' with ie" do
+        "ied"
+        |> Porter2.Word.replace_suffixes
+        |> expect |> to( eq "ie" )
+      end
+
+      it "should replace the suffix with 'ie' if there is only one letter before it" do
+        "tied"
+        |> Porter2.Word.replace_suffixes
+        |> expect |> to( eq "tie" )
+      end
+
+      it "should replace the suffix with 'i' if there is more than one letter before it" do
+        "tried"
+        |> Porter2.Word.replace_suffixes
+        |> expect |> to( eq "tri" )
+      end
+    end
+
+    context "words with an 'ies' suffix" do
+      it "should replace the word 'ies' with ie" do
+        "ies"
+        |> Porter2.Word.replace_suffixes
+        |> expect |> to( eq "ie" )
+      end
+
+      it "should replace the suffix with 'ie' if there is only one letter before it" do
+        "ties"
+        |> Porter2.Word.replace_suffixes
+        |> expect |> to( eq "tie" )
+      end
+
+      it "should replace the suffix with 'i' if there is more than one letter before it" do
+        "tries"
+        |> Porter2.Word.replace_suffixes
+        |> expect |> to( eq "tri" )
+      end
+    end
+
+  
+    context "words with an 's' suffix" do
+      it "should remove the suffix if there is a vowel in the word that is not directly preceeding the suffix" do
+        "gaps"
+        |> Porter2.Word.replace_suffixes
+        |> expect |> to( eq "gap" )
+      end
+
+      it "should remove the suffix if there is a vowel in the word that is not directly preceeding the suffix, but also a vowel that directly preceeds the suffix" do
+        "kiwis" 
+        |> Porter2.Word.replace_suffixes
+        |> expect |> to( eq "kiwi" )
+      end
+
+      it "should not change the word if there is no vowel in the word that is not directly preceeding the suffix" do
+        "bcs"
+        |> Porter2.Word.replace_suffixes
+        |> expect |> to( eq "bcs" )
+      end
+
+      it "should not change the word if there is a vowel directly preceeding the suffix, but not before" do
+        "gas"
+        |> Porter2.Word.replace_suffixes
+        |> expect |> to( eq "gas" )
+      end
+    end
+
+    it "should replace an 'eed' suffix with 'ee' if it is in the r1 region"
+    it "should not replace an 'eed' suffix if it is not in the r1 region"
+    it "should replace an 'eedly' suffix with 'ee' if it is in the r1 region"
+    it "should not replace an 'eedly' suffix with 'ee' if it is not in the r1 region"
+
+    context "word with an 'edly' suffix" do
+      context "when the preceeding word part contains a vowel" do
+        it "should replace the suffix with 'e' if the suffix is preceeded by 'at'" do
+          "luxuriatedly"
+          |> Porter2.Word.replace_suffixes
+          |> expect |> to( eq "luxuriate" )
+        end
+
+        it "should replace the suffix with 'e' if the suffix is preceeded by 'bl'" do
+          "somethingbledly"
+          |> Porter2.Word.replace_suffixes
+          |> expect |> to( eq "somethingble" )
+        end
+
+        it "should replace the suffix with 'e' if the suffix is preceeded by 'iz'" do
+          "somethingizedly"
+          |> Porter2.Word.replace_suffixes
+          |> expect |> to( eq "somethingize" )
+        end
+
+        it "should remove the suffix and remove one of a doubled letter if the suffix is preceeded by a doubled letter" do
+          "hoppedly"
+          |> Porter2.Word.replace_suffixes
+          |> expect |> to( eq "hop" )
+        end
+
+        it "should replace the suffix with an 'e' if the word without the suffix is short" do
+          "hopedly"
+          |> Porter2.Word.replace_suffixes
+          |> expect |> to( eq "hope" )
+        end
+
+        it "otherwise it should just remove the suffix" do
+          "awkwardedly"
+          |> Porter2.Word.replace_suffixes
+          |> expect |> to( eq "awkward" )
+        end
+      end
+
+      it "should not change the word if the preceeding word part does not contain a vowel" do
+        "medly"
+        |> Porter2.Word.replace_suffixes
+        |> expect |> to( eq "medly" )
+      end
+
+      it "should not change the word if there is no preceeding word part" do
+        "edly"
+        |> Porter2.Word.replace_suffixes
+        |> expect |> to( eq "edly" )
+      end
+    end
+
+    it "should replace an 'ing' suffix with 'e' if the suffix is preceeded by 'at'"
+    it "should replace an 'ing' suffix with 'e' if the suffix is preceeded by 'bl'"
+    it "should replace an 'ing' suffix with 'e' if the suffix is preceeded by 'iz'"
+    it "should remove an 'ing' suffix and remove one of a doubled letter if the suffix is preceeded by a doubled letter"
+    it "should replace an 'ing' suffix with an 'e' if the word without the suffix is short"
+    it "should remove an 'ing' suffix if it is not preceeded by 'at', 'bl' or 'iz'"
+
+    it "should replace an 'ingly' suffix with 'e' if the suffix is preceeded by 'at'"
+    it "should replace an 'ingly' suffix with 'e' if the suffix is preceeded by 'bl'"
+    it "should replace an 'ingly' suffix with 'e' if the suffix is preceeded by 'iz'"
+    it "should remove an 'ingly' suffix and remove one of a doubled letter if the suffix is preceeded by a doubled letter"
+    it "should replace an 'ingly' suffix with an 'e' if the word without the suffix is short"
+    it "should remove an 'ingly' suffix if it is not preceeded by 'at', 'bl' or 'iz'"
+
+    it "should replace an 'ed' suffix with 'e' if the suffix is preceeded by 'at'"
+    it "should replace an 'ed' suffix with 'e' if the suffix is preceeded by 'bl'"
+    it "should replace an 'ed' suffix with 'e' if the suffix is preceeded by 'iz'"
+    it "should remove an 'ed' suffix and remove one of a doubled letter if the suffix is preceeded by a doubled letter"
+    it "should replace an 'ed' suffix with an 'e' if the word without the suffix is short"
+    it "should remove an 'ed' suffix if it is not preceeded by 'at', 'bl' or 'iz'"
+
+
+    it "should replace a 'y' suffix with an 'i' if it is preceeded by a non-vowel and the word is longer than 2 letters"
+    it "should not replace a 'y' suffix if the suffix is not preceeded by a non-vowel"
+    it "should not replace a 'y' suffix if the word is only 2 letters long and the suffix is preceeded by a vowel"
+    it "should not replace a 'y' suffix if the word is only 2 letters long and the suffix is preceeded by a non-vowel"
+
+    it "should replace a 'Y' suffix with an 'i' if it is preceeded by a non-vowel and the word is longer than 2 letters"
+    it "should not replace a 'Y' suffix if the suffix is not preceeded by a non-vowel"
+    it "should not replace a 'Y' suffix if the word is only 2 letters long and the suffix is preceeded by a vowel"
+    it "should not replace a 'Y' suffix if the word is only 2 letters long and the suffix is preceeded by a non-vowel"
+  end
+
+  context ".r1_region" do
+    context "for a word that contains a non-vowel following a vowel" do
+      it "should return the word part after this non-vowel" do
+        "foobar"
+        |> Porter2.Word.r1_region
+        |> expect |> to( eq "ar" )
+      end
+    end
+
+    context "for a word that as no non-vowel following a vowel" do
+      it "should return nil" do
+        "foo"
+        |> Porter2.Word.r1_region
+        |> expect |> to( eq nil )
+      end
+    end
+
+    context "for a word that has no word part following the vowel <> non-vowel sequence" do
+      it "should return nil" do
+        "it"
+        |> Porter2.Word.r1_region
+        |> expect |> to( eq nil )
+      end
+    end
+
+    context "for a word that has no vowel" do
+      it "should return nil" do
+        "psst"
+        |> Porter2.Word.r1_region
+        |> expect |> to( eq nil )
+      end
+    end
+  end
+
+  context ".word_ends_in_short_syllable?" do
+    context "for a word with a suffix of the type 'non-vowel' <> 'vowel' <> 'non-vowel other than w, x or Y'" do
+      it "should return true" do
+        "tetris"
+        |> Porter2.Word.word_ends_in_short_syllable?
+        |> expect |> to( be_true )
+      end
+    end
+
+    context "for a word with a suffix of the type 'non-vowel' <> 'vowel' <> 'w'" do
+      it "should return false" do
+        "now"
+        |> Porter2.Word.word_ends_in_short_syllable?
+        |> expect |> to( be_false )
+      end
+    end
+
+    context "for a word with a suffix of the type 'non-vowel' <> 'vowel' <> 'x'" do
+      it "should return false" do
+        "sex"
+        |> Porter2.Word.word_ends_in_short_syllable?
+        |> expect |> to( be_false )
+      end
+    end
+
+    context "for a word with a suffix of the type 'non-vowel' <> 'vowel' <> 'Y'" do
+      it "should return false" do
+        "feY"
+        |> Porter2.Word.word_ends_in_short_syllable?
+        |> expect |> to( be_false )
+      end
+    end
+
+    context "for a word with a suffix of the type 'non-vowel' <> 'vowel'" do
+      it "should return false" do
+        "seemingly"
+        |> Porter2.Word.word_ends_in_short_syllable?
+        |> expect |> to( be_false )
+      end
+    end
+
+    context "for a word with a suffix of the type 'non-vowel' <> 'non-vowel'" do
+      it "should return false" do
+        "ring"
+        |> Porter2.Word.word_ends_in_short_syllable?
+        |> expect |> to( be_false )
+      end
+    end
+
+    context "for a word containing only one letter" do
+      it "should return false" do
+        "a"
+        |> Porter2.Word.word_ends_in_short_syllable?
+        |> expect |> to( be_false )
+      end
+    end
+  end
+
+  context ".is_word_short?" do
+    context "if the word ends in a short syllable and r1_region is nil" do
+      it "should return true" do
+        "spin"
+        |> Porter2.Word.is_word_short?
+        |> expect |> to( be_true )
+      end
+    end
+
+    # NOTE: if the r1 region is not nil, the word cannot end in a short syllable
+    
+    context "if the word does not end in a short syllable and r1_region is nil" do
+      it "should return false" do
+        "sex"
+        |> Porter2.Word.is_word_short?
+        |> expect |> to( be_false )
+      end
+    end
+
+    context "if the word does not end in a short syllable and r1_region is not nil" do
+      it "should return false" do
+        "ring"
+        |> Porter2.Word.is_word_short?\
+        |> expect |> to( be_false )
+      end
+    end
+  end
 end
