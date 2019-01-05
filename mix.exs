e6314a02f920eb884839efa62ee3e4b5bdbc662d
Sven Riedel <sr@gimp.org>
2019-01-05 11:40:09 +0100

    Add exdoc to dependencies to satisfy hex

--- mix.exs
+++ mix.exs
@@ -46,7 +46,8 @@ defmodule Porter2.Mixfile do
   defp deps do
     [ {:espec, "1.6.3", only: :test},
       {:credo, "~> 0.5", only: [ :test, :dev ] },
-      {:coverex, "~> 1.4.10", only: :test }
+      {:coverex, "~> 1.4.10", only: :test },
+      {:exdoc, ">= 0.0.0", only: :dev }
     ]
   end
 end
