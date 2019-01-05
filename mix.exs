fe01e1a9ebe09f14e1488377e3c9c0be7cc5de55
Sven Riedel <sr@gimp.org>
2019-01-05 11:37:00 +0100

    Bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Porter2.Mixfile do
 
   def project do
     [app: :porter2,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.7",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
